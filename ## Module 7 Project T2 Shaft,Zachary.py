2d81cfc2d25dd732dfa336f8ae4b5a96234c0d7b
Melcior <101022486+markgodiy@users.noreply.github.com>
2023-12-13 19:07:50 -0500

    Update ## Module 7 Project T2 Shaft,Zachary.py

--- ## Module 7 Project T2 Shaft,Zachary.py
+++ ## Module 7 Project T2 Shaft,Zachary.py
@@ -1,5 +1,5 @@
 def hillClimb(arr, start_index):
-    if not arr or start_index < 0 or start_index >= len(arr):
+    if not arr or start_index < 0 or start_index >= len(arr) - 1:
         return None, None
 
     current_index = start_index
@@ -7,50 +7,33 @@ def hillClimb(arr, start_index):
     while True:
         current_value = arr[current_index]
 
-        left_index = current_index - 1 if current_index > 0 else None
-        right_index = current_index + 1 if current_index < len(arr) - 1 else None
+        left_index = current_index - 1
+        right_index = current_index + 1
 
-        left_value = arr[left_index] if left_index is not None else float('-inf')
-        right_value = arr[right_index] if right_index is not None else float('-inf')
+        left_value = arr[left_index] if left_index >= 0 else float('-inf')
+        right_value = arr[right_index] if right_index < len(arr) else float('-inf')
 
-        print(f"Current Index: {current_index}, Current Value: {current_value}")
-        print(f"Left Index: {left_index}, Left Value: {left_value}")
-        print(f"Right Index: {right_index}, Right Value: {right_value}")
+        # Print current state for debugging
+        print(f"L({left_value}) [{current_index}]={current_value} R({right_value})")
 
-        # Check for peak: Three consecutive numbers are equal
-        if (left_value == current_value) and (current_value == right_value):
-            print("Found local maximum.")
-            return current_index, current_value
+        # Check if the current value is greater than or equal to its neighbors
+        if current_value >= left_value and current_value >= right_value:
+            # check two neighbors to see if they are greater
+            if current_index + 2 < len(arr) and arr[current_index + 2] > arr[current_index + 1]:
+                current_index += 2
+                continue
+            # check two neighbors to see if they are less
+            elif current_index - 2 >= 0 and arr[current_index - 2] > arr[current_index - 1]:
+                current_index -= 2
+                continue
+            else: 
+                print("Found local maximum.")
+                return current_index, current_value
 
-        # Move towards the higher neighbor
-        if right_value > left_value:
-            print("Moving right.")
-            current_index = right_index
-        else:
-            print("Moving left.")
-            current_index = left_index
+        # Move right if right_value is greater, or left if not
+        current_index = right_index if right_value > left_value else left_index
 
 # Example usage:
 my_array = [6, 5, 5, 5, 4, 3, 2]
 result_index, result_value = hillClimb(my_array, 5)
 print(f"Local maximum index: {result_index}, Value: {result_value}")
-
-
-# python.exe .\hillclimb.py
-# Current Index: 5, Current Value: 3
-# Left Index: 4, Left Value: 4
-# Right Index: 6, Right Value: 2
-# Moving left.
-# Current Index: 4, Current Value: 4
-# Left Index: 3, Left Value: 5
-# Right Index: 5, Right Value: 3
-# Moving left.
-# Current Index: 3, Current Value: 5
-# Left Index: 2, Left Value: 5
-# Right Index: 4, Right Value: 4
-# Moving left.
-# Current Index: 2, Current Value: 5
-# Left Index: 1, Left Value: 5
-# Right Index: 3, Right Value: 5
-# Found local maximum.
-# Local maximum index: 2, Value: 5
